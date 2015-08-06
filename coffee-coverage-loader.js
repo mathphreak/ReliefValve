559709cfc1819938985b9319cd0dc9f626f37b59
Matt Horn <mathphreak@gmail.com>
2015-08-05 21:24:23 -0600

    Ignore UI code for test coverage purposes

---
+++ coffee-coverage-loader.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var coffeeCoverage = require('coffee-coverage');
+var projectRoot = path.normalize(__dirname);
+var coverageVar = coffeeCoverage.findIstanbulVariable();
+// Only write a coverage report if we're not running inside of Istanbul.
+var writeOnExit = (coverageVar == null) ? (projectRoot + '/coverage/coverage-coffee.json') : null;
+
+coffeeCoverage.register({
+    instrumentor: 'istanbul',
+    basePath: projectRoot,
+    exclude: [
+      '/test',
+      '/node_modules',
+      '/.git',
+      '/gulpfile.coffee',
+      '/' + path.normalize('src/client.coffee'),
+      '/' + path.normalize('src/main.coffee')
+    ],
+    coverageVar: coverageVar,
+    writeOnExit: writeOnExit,
+    initAll: true
+});
